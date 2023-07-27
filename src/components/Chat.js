d10ce72f00b0097406bda7d9fa6265a5844d0977
SukhdeepSingh1234 <ss6540029@gmail.com>
2023-07-27 11:16:04 +0530

    frontend done

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Chat.css'
 import { Avatar,IconButton } from '@mui/material'
 import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material'
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
+import MicIcon from '@mui/icons-material/Mic';
 
 
 function Chat() {
+  const [text,setText]=useState('')
   return (
     <div className='chat'>
       <div className='chat_header'>
@@ -31,6 +34,29 @@ function Chat() {
             this is a message
             <span className='chat_timestamp' >{new Date().toUTCString()}</span>
           </p>
+          <p className='chat_message chat_receiver' >
+            <span className='chat_name'>Sukhdeep</span>
+            this is a message
+            <span className='chat_timestamp' >{new Date().toUTCString()}</span>
+          </p>
+          <p className='chat_message' >
+            <span className='chat_name'>Sukhdeep</span>
+            this is a message
+            <span className='chat_timestamp' >{new Date().toUTCString()}</span>
+          </p>
+        
+      </div>
+      <div className='chat_footer' >
+        <IconButton>
+           <EmojiEmotionsOutlinedIcon style={{ color: "gray" }}  />
+        </IconButton>
+        <div className='chat_typemsg' >
+          <input type="text" placeholder='Type a message' onChange={e=> setText(e.target.value)} name="msg" value={text} />
+        </div>
+
+        <IconButton>
+            <MicIcon />
+        </IconButton>
         
       </div>
     </div>
