1cc05d2c89b725445e37cd2a34d0cd300d294400
SukhdeepSingh1234 <ss6540029@gmail.com>
2023-07-27 22:27:44 +0530

    backend almost done

--- backend/dbMessages.js
+++ backend/dbMessages.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const whatsappSchema=mongoose.Schema({
+const whatsappSchema=new mongoose.Schema({
         message:String,
         name:String,
         timestamp:String,
         received:Boolean,
 })
-
-export default mongoose.model('messageContent',whatsappSchema)
+// creating collection messageCollection
+export default mongoose.model('messagecontent',whatsappSchema)
--- backend/server.js
+++ backend/server.js
@@ -2,44 +2,82 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js"
-import axios from "axios";
+import axios from "./axios.js";
+import Pusher from "pusher";
 
 //config
 const app=express();
 const port=process.env.PORT || 9000
 
+const pusher = new Pusher({
+    appId: "1642033",
+    key: "2f97fad1caa7f16732ce",
+    secret: "d3387288a7d27f6f6368",
+    cluster: "ap2",
+    useTLS: true
+  });
 // middlewares
 
 app.use(express.json())
+
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*")
+    res.setHeader("Access-Control-Allow-Headers","*")
+    next()
+})
 
 // dbconfig
 const connection_url= 'mongodb+srv://admin:EzY7OT9bfJxyGYYh@cluster0.6mv9wz7.mongodb.net/whatsappDb?retryWrites=true&w=majority'
 mongoose.connect(connection_url)
 
+// configuring Pusher to get real time database feature
+const db=mongoose.connection;
+db.once('open',()=>{
+    console.log("DB Connected")
+    // remember to put "s" at the end of the collection as mongoose automatically creates a plural collection else we will be unable to detect
+    const msgCollection=db.collection("messagecontents")// here the collection name should be the same we created before so that the pusher listens to that particular collection for changes in real time
+    const changeStream=msgCollection.watch(); // here pusher watches for the change in collection
 
+    changeStream.on('change',(change)=>{ // if change happens in the stream then invoke the function
+        console.log(change)
+        if(change.operationType==='insert'){ // operationType is a status json that tells if inserted or not if yes
+            const messageDetails=change.fullDocument;// then get the full document from the json that contains data of message and name,..etc parameter we gave in the body
+            pusher.trigger('messages','inserted', // messages will be the server that checks for change inserted and trigger the pusher
+            {
+                name:messageDetails.name,
+                message:messageDetails.message
+            })
+        }else{
+            console.log("Error triggering Pusher")
+        }
+    })
+})
 
 // api routes
 
+
 app.get('/',(req,res)=>{
-    res.status(200).send("hello world")
+    res.status(200).send("hello world it works")
 })
 
-// axios.post('/messages/new')
-//     .then(function (data) {
-//         console.log(data);
-//     }, (error) => {
-//         console.log(error);
-//     })
-
-app.post('/messages/new',async(res,req)=>{
-    const dbMessage=req.body;
-
-    await Messages.create(dbMessage)
+app.get('/messages/sync',(req,res)=>{
+    Messages.find({})
         .then((data)=>{
-            res.status(500).send(data)
+            res.status(201).send(data)
         })
         .catch((err)=>{
-            res.status(201).send(err)
+            res.status(500).send(err)
+        })        
+})
+
+app.post('/messages/new',(req,res)=>{
+    const dbMessage=new Messages(req.body);
+    Messages.create(dbMessage)
+        .then((data)=>{
+            res.status(201).send(data)
+        })
+        .catch((err)=>{
+            res.status(500).send(err)
         })          
 })
     
