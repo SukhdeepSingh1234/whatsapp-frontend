bc9874805939f31ee8689956cb8feea4cb21f078
SukhdeepSingh1234 <ss6540029@gmail.com>
2024-06-07 11:35:14 +0530

    resolved home page bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-import { useEffect, useState,} from 'react';
 import Login from './components/Login'
 import { Navigate, Route, Routes } from 'react-router-dom';
 import OtpVerify from './components/OtpVerify';
 import { BrowserRouter} from 'react-router-dom';
 import Application from './components/Application';
 import SetProfile from './components/SetProfile';
-import Home from './components/Home'
+
 
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
       <div className='app_body' >
       <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={authToken? <Navigate to="/Application"/> : <Home />} />
         <Route exact path="/Login" element={authToken? <Navigate to="/Application"/> : <Login/>} />
         <Route exact path="/OtpVerify" element={authToken? <Navigate to="/Application"/> : <OtpVerify />} />
         <Route exact path="/SetProfile" element={ authToken? <Navigate to="/Application"/> : <SetProfile />} />
